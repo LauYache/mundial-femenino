1d3fcd61933dca07f4807d79017e99f368f96d36
LauYache <86937668+LauYache@users.noreply.github.com>
2024-05-27 10:19:35 -0300

    updating features

--- Torneo/src/Main.java
+++ Torneo/src/Main.java
@@ -1,16 +1,13 @@
+import java.util.LinkedList;
 import javax.swing.JOptionPane;
-
 class Main {
-
 	public static void main(String[] args) {
-
 		//		Creación de una clase principal "Main" para probar el sistema:
 		//			Crear instancias de jugadores, equipos y el gestor de equipos.
 		//			Realizar pruebas de todas las operaciones implementadas, cómo agregar jugadores a un equipo, buscar un jugador por nombre, eliminar un equipo, etc.
 		//			Simular partidos entre equipos utilizando el método "jugarPartido".
 		//			Entrega:
 		//			Deberán entregar el código fuente del proyecto, junto con un diagrama de clases que representará, las funcionalidades implementadas. 
-		//	
 
 		String[] menuPpal = {
 				"Gestionar equipos","Gestionar torneo","Salir"	
@@ -21,22 +18,33 @@ class Main {
 		String[] menuTorneo = {
 				"Eliminar equipo", "Agregar equipo","Buscar equipo", "Mostrar cantidad de equipos",  "Mostrar lista de equipos","Jugar partido", "Salir"	
 			};
+		LinkedList<Equipo> equiposTorneo = new LinkedList<Equipo>();
+		
+		GestionTorneo nuevoTorneo = new GestionTorneo(equiposTorneo);
 		
 		int opcionPpal;
 		int opcionEquipos;
 		int opcionTorneo;
+		int equipoElegido;
 		do {
 			opcionPpal = JOptionPane.showOptionDialog(null, "Elija una opcion", null, 0, 0, null, menuPpal, menuPpal[0]);
 			switch (opcionPpal) {
 			//Gestion equipos
 			case 0:	
+				if (equiposTorneo.isEmpty()){
+					JOptionPane.showMessageDialog(null, "Vuelva al inicio y agregue equipos al torneo");
+				}else {
+				
 				do {
-				opcionEquipos = JOptionPane.showOptionDialog(null, "Elija una opcion", null, 0, 0, null, menuEquipos, menuEquipos[0]);
+			
+					
+					opcionEquipos = JOptionPane.showOptionDialog(null, "Elija una opcion", null, 0, 0, null, menuEquipos, menuEquipos[0]);
+				
 				
 				switch (opcionEquipos) {
 					case 0:
 					//Menu para seleccionar el equipo
-		
+						equipoElegido = JOptionPane.showOptionDialog(null, "Seleccione equipo", null, 0, 0, null, menuPpal, menuPpal[0]);
 							break;
 						case 1:
 							//eliminar jugador
@@ -60,16 +68,20 @@ class Main {
 							break;
 					
 				}
+					
+				
 
 				}while (opcionEquipos!=6);
+				}
+				
 				
 				break;
 				//Gestion torneo
 			case 1:
 					do {
-						opcionEquipos = JOptionPane.showOptionDialog(null, "Elija una opcion", null, 0, 0, null, menuEquipos, menuEquipos[0]);
+						opcionTorneo = JOptionPane.showOptionDialog(null, "Elija una opcion", null, 0, 0, null, menuTorneo, menuTorneo[0]);
 						
-						switch (opcionEquipos) {
+						switch (opcionTorneo) {
 							case 0:
 							
 								//eliminar equipo
@@ -97,7 +109,7 @@ class Main {
 							
 						}
 
-						}while (opcionEquipos!=6);
+						}while (opcionTorneo!=6);
 
 					break;
 						
